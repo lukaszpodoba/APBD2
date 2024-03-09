0450a86bbdbc63bf9678367daafee58738cec088
s27478 <s27478@pjwstk.edu.pl>
2024-03-09 19:09:29 +0100

    Nowe zadanie

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine(GetAvg(new []{1,2,3,4,5}));
+
+static double GetAvg(int[] arr)
+{
+    double suma = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        suma += arr[i];
+    }
+    return suma / arr.Length;
+}
