fbbf419c8f6d8b38b6889900ac2f482c9dc807d2
s27478 <s27478@pjwstk.edu.pl>
2024-03-09 19:21:54 +0100

    nowe zadanie 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(GetAvg(new []{1,2,3,4,5}));
+Console.WriteLine(getAvg(new []{1,2,3,4,5}));
+Console.WriteLine(getMax(new []{1,2,3,4,5}));
 
 static double GetAvg(int[] arr)
 {
@@ -10,4 +11,17 @@ static double GetAvg(int[] arr)
         suma += arr[i];
     }
     return suma / arr.Length;
+}
+
+static int GetMax(int[] arr)
+{
+    int max = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= max)
+        {
+            max = arr[i];
+        } 
+    }
+    return max;
 }
